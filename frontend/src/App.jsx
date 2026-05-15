7ad0ad4ddd772b6c2095c6e49b8431461966c6ca
Sewak Dhakal <sew007ak@gmail.com>
2026-05-15 18:30:22 +0545

    App.jsx modified

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import SignUp from "./pages/Signup.jsx";
 import Login from "./pages/Login.jsx";
 import AdminDashboard from './pages/AdminDashboard.jsx';
 import AddProduct from './pages/AddProduct.jsx';
+import Aboutus from './pages/Aboutus.jsx';
+import PrivacyPage from './pages/PrivacyPage.jsx';
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/admin-dashboard' element={<AdminDashboard />} />
       <Route path='/admin-addProduct' element={<AddProduct />} />
+      <Route path='/about-us' element={<Aboutus />} />
+      <Route path='/privacy-policy' element={<PrivacyPage />} />
     </Routes>
   )
 }
